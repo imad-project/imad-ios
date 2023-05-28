55e88f946bf778d422282166d2cc74e2282dec14
QuaRang1225 <dbduddnd1225@gmail.com>
2023-05-29 02:30:00 +0900

    영화 장르필터 추가

--- IMAD_Project/Filter/GenreFilter.swift
+++ IMAD_Project/Filter/GenreFilter.swift
@@ -7,46 +7,68 @@
 
 import Foundation
 
-enum GenreFilter:String, CaseIterable{
+enum MovieGenreFilter:String, CaseIterable{
     case family
     case fantasy
     case horror
     case crime
     case SF
     case comedy
-    case world
     case documentary
     case action
     case romance
     case music
     case classic
+    case adventure
+    case animation
+    case drama
+    case mystery
+    case movie
+    case thriller
+    case war
+    case western
+    
     
     var generName:String{
         switch self{
         case .family:
-            return "가족/키즈"
+            return "가족"
         case .fantasy:
-            return "판타지/마법"
+            return "판타지"
         case .horror:
-            return "공포/호러"
+            return "공포"
         case .crime:
-            return "범죄/추리"
+            return "범죄"
         case .SF:
-            return "SF/공상과학"
+            return "SF"
         case .comedy:
-            return "코미디/개그"
-        case .world:
-            return "해외"
+            return "코미디"
         case .documentary:
             return "다큐멘터리"
         case .action:
-            return "액션/누아르"
+            return "액션"
         case .romance:
-            return "로맨스/애로"
+            return "로맨스"
         case .music:
-            return "뮤직"
+            return "음악"
         case .classic:
-            return "고전/신화"
+            return "역사"
+        case .adventure:
+            return "모험"
+        case .animation:
+            return "애니메이션"
+        case .drama:
+            return "드라마"
+        case .mystery:
+            return "미스터리"
+        case .movie:
+            return "TV 영화"
+        case .thriller:
+            return "스릴러"
+        case .war:
+            return "전쟁"
+        case .western:
+            return "서부"
         }
     }
 }
