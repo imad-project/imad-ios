8dfcaa6a09ff62bc7f7d3fc0eb8c67ec7fab63d3
QuaRang1225 <dbduddnd1225@gmail.com>
2024-01-08 15:54:06 +0900

    #89 - 본문 글자 수 제한
    - 커스텀 텍스트 에디터 생성
    - 리뷰 및 게시물 작성 시 글자 수 제한 (2500)

---
+++ IMAD_Project/Core/Custom/View/CustomTextEditor.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextEditor.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 1/8/24.
+//
+
+import SwiftUI
+
+struct CustomTextEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextEditor()
+}
