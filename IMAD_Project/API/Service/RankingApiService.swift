264ae93d1e0a35ce5bea9e2ff051b1396d8a64c7
QuaRang1225 <dbduddnd1225@gmail.com>
2024-03-08 09:33:37 +0900

    #99 - 오늘의 리뷰 및 게시물
    - DTO 구조체 작성
    - data부는 값의 변경을 우려해 var로 선언

--- IMAD_Project/API/Service/RankingApiService.swift
+++ IMAD_Project/API/Service/RankingApiService.swift
@@ -52,4 +52,7 @@ class RankingApiService{
             }
             .eraseToAnyPublisher()
     }
+//    static func popularReview() -> AnyPublisher<>{
+//        
+//    }
 }
---
+++ IMAD_Project/Model/Ranking/PopularReviewResponse.swift
@@ -0,0 +1,39 @@
+//
+//  PopularReviewResponse.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 3/8/24.
+//
+
+import Foundation
+
+struct PopularReviewResponse:Codable{
+    let reviewID, contentsID: Int
+    let contentsTitle, contentsPosterPath: String
+    let userID: Int
+    let userNickname: String
+    let userProfileImage: Int
+    let title, content: String
+    let score: Double
+    let likeCnt, dislikeCnt: Int
+    let createdAt, modifiedAt: String
+    let likeStatus: Int
+    let author, spoiler: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case reviewID = "review_id"
+        case contentsID = "contents_id"
+        case contentsTitle = "contents_title"
+        case contentsPosterPath = "contents_poster_path"
+        case userID = "user_id"
+        case userNickname = "user_nickname"
+        case userProfileImage = "user_profile_image"
+        case title, content, score
+        case likeCnt = "like_cnt"
+        case dislikeCnt = "dislike_cnt"
+        case createdAt = "created_at"
+        case modifiedAt = "modified_at"
+        case likeStatus = "like_status"
+        case author, spoiler
+    }
+}
