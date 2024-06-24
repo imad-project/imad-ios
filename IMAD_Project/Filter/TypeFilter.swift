6748364014be1f89acfb5a864e5efa75f0abe2c6
QuaRang <dbduddnd1225@gmail.com>
2024-06-24 22:12:28 +0900

    #105 - backdrop이미지 추가
    - 메인화면을 꾸며줄 backdrop이미지 응답값에 추가

---
+++ IMAD_Project/Filter/TypeFilter.swift
@@ -0,0 +1,8 @@
+//
+//  TypeFilter.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 6/24/24.
+//
+
+import Foundation
--- IMAD_Project/Model/Recommend/RecommendResponse.swift
+++ IMAD_Project/Model/Recommend/RecommendResponse.swift
@@ -7,13 +7,15 @@
 
 import Foundation
 
-struct RecommendResponse: Codable {
+struct RecommendResponse: Codable,Hashable {
     let id: Int
     let name: String?
     let posterPath: String?
+    let backdropPath:String?
 
     enum CodingKeys: String, CodingKey {
         case id, name
         case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
     }
 }
