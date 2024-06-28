24ea42dc9164b5e5bf00edc654eac0a7d4e214a2
QuaRang <dbduddnd1225@gmail.com>
2024-06-28 21:13:32 +0900

    #106 -  프로토콜 타입/\ForEach문 관련
    - ForEach문의 구조를 파본 결과 3가지가 있어야함
    - 실제로 받을 데이터와의 데이터 바인딩을 위한 변수
    - 실제 반복 시킬 데이터 배열 변수
    - 데이터로 뷰를 만들 클로져 변수
    - 각각을 만들어 ForEach문과 함께 사용하니 문제 해결

--- IMAD_Project/Core/Custom/View/Elements/ListView.swift
+++ IMAD_Project/Core/Custom/View/Elements/ListView.swift
@@ -11,19 +11,19 @@ public struct ListView<RefreshBinding, Data, ItemView: View>: View {
     
     @Binding var binding: RefreshBinding
     let items: [Data]
-    var viewMapping: (Data) -> ItemView
+    @ViewBuilder var viewMapping: (Data) -> ItemView
     
     
-    //  public init(
-    //              binding: Binding<RefreshBinding>,
-    //              items: [Data],
-    //              @ViewBuilder viewMapping: @escaping (Data) -> ItemView) {
-    //
-    //    _binding = binding
-    //    self.items = items
-    //    self.viewMapping = viewMapping
-    //
-    //  }
+      public init(
+                  binding: Binding<RefreshBinding>,
+                  items: [Data],
+                  @ViewBuilder viewMapping: @escaping (Data) -> ItemView) {
+    
+        _binding = binding
+        self.items = items
+        self.viewMapping = viewMapping
+    
+      }
     
     public var body: some View {
         ForEach(Array(items.enumerated()), id: \.offset) { index, item in
