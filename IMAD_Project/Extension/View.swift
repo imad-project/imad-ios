ace9bdcbbea8af5bfebc5ff9ab02a4bce14a41b7
QuaRang <dbduddnd1225@gmail.com>
2024-07-05 21:34:14 +0900

    #112 - UIImage,View 관련
    - 각각 확장자 파일을 만들어 기존 extasion 코드 추가

--- IMAD_Project/Extension/View.swift
+++ IMAD_Project/Extension/View.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension View{
+    @ViewBuilder
+    func cropImagePicker(show:Binding<Bool>,croppedImage:Binding<UIImage?>)->some View{
+        CustomImagePicker(show: show, croppedImage: croppedImage) {
+            self
+        }
+    }
+    
+    @ViewBuilder
+    func frame(_ size:CGSize)-> some View{
+        self
+            .frame(width:size.width,height: size.height)
+    }
+    var mainWidth:CGFloat{
+        UIScreen.main.bounds.width
+    }
+    var mainHeight:CGFloat{
+        UIScreen.main.bounds.height
+    }
+    func isPad()->Bool{
+        UIDevice.current.userInterfaceIdiom == .pad
+    }
+    func haptics(_ style:UIImpactFeedbackGenerator.FeedbackStyle){
+        UIImpactFeedbackGenerator(style: style).impactOccurred()
+    }
+}
