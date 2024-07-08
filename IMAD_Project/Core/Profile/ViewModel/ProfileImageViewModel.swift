5ed94b76b97e9ef47890b2c31d27ba43a6c2776f
QuaRang <dbduddnd1225@gmail.com>
2024-07-08 14:04:38 +0900

    #114 - 프로필 이미지 Viewmodel
    - 프로필 이미지 ApiService를 토대로 실제로 앱과 상호작용을 할 ViewModel생성
    - 데이터를 요청하고 응답 받는 기능만을 하는 메서드

--- IMAD_Project/Core/Profile/ViewModel/ProfileImageViewModel.swift
+++ IMAD_Project/Core/Profile/ViewModel/ProfileImageViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import Combine
+
+class ProfileImageViewModel:ObservableObject{
+    
+    var refreschTokenExpired = PassthroughSubject<(),Never>()
+    var cancelable = Set<AnyCancellable>()
+    @Published var message = ""
+    
+    func fetchProfileImageCustom(image:Data){
+        ProfileImageApiService.fetchProfileImageCustom(image: image)
+            .sink { completion in
+                switch completion{
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.refreschTokenExpired.send()
+                case .finished:
+                    print(completion)
+                }
+            } receiveValue: { [weak self] noData in
+                self?.message = noData.message
+            }.store(in: &cancelable)
+    }
+    func fetchProfileImageDefault(image:String){
+        ProfileImageApiService.fetchProfileImageDefault(image: image)
+            .sink { completion in
+                switch completion{
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.refreschTokenExpired.send()
+                case .finished:
+                    print(completion)
+                }
+            } receiveValue: { [weak self] noData in
+                self?.message = noData.message
+            }.store(in: &cancelable)
+    }
+}
