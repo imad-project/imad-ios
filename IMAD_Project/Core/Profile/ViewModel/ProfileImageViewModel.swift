6d1246d666c4278a1b33c3b2a148af5f87797bf8
QuaRang <dbduddnd1225@gmail.com>
2024-07-08 17:57:27 +0900

    #114 - 뷰모델 및 클래스 정리
    - 유저 서비스에서 이미지 코드를 이미지 링크로 변경
    - 쓸데 없는 로그 삭제

--- IMAD_Project/Core/Profile/ViewModel/ProfileImageViewModel.swift
+++ IMAD_Project/Core/Profile/ViewModel/ProfileImageViewModel.swift
@@ -13,10 +13,13 @@ class ProfileImageViewModel:ObservableObject{
     var refreschTokenExpired = PassthroughSubject<(),Never>()
     var cancelable = Set<AnyCancellable>()
     @Published var message = ""
+    @Published var customImage:Data?
+    @Published var defaultImage:ProfileFilter = .indigo
     
     func fetchProfileImageCustom(image:Data){
         ProfileImageApiService.fetchProfileImageCustom(image: image)
             .sink { completion in
+                print(completion)
                 switch completion{
                 case .failure(let error):
                     print(error.localizedDescription)
@@ -31,13 +34,14 @@ class ProfileImageViewModel:ObservableObject{
     func fetchProfileImageDefault(image:String){
         ProfileImageApiService.fetchProfileImageDefault(image: image)
             .sink { completion in
-                switch completion{
-                case .failure(let error):
-                    print(error.localizedDescription)
-                    self.refreschTokenExpired.send()
-                case .finished:
-                    print(completion)
-                }
+                print(completion)
+//                switch completion{
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                    self.refreschTokenExpired.send()
+//                case .finished:
+//                    print(completion)
+//                }
             } receiveValue: { [weak self] noData in
                 self?.message = noData.message
             }.store(in: &cancelable)
