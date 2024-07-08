5103a176b252449a79c46e453a828450d3469a32
QuaRang <dbduddnd1225@gmail.com>
2024-07-08 13:55:44 +0900

    #114 - 프로필 이미지 요청 관련
    - API 서비스를 구현해 각 요청 모듈화
    - 커스텀의 경우 이미지 파일, 이름, 형태 등을 정의
    - 기본인 경우 텍스트를 utf8로 디코딩 하여 저장

--- IMAD_Project/API/Service/ProfileImageApiService.swift
+++ IMAD_Project/API/Service/ProfileImageApiService.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import Alamofire
+import Combine
+
+class ProfileImageApiService{
+    
+    static let intercept = FormDataInterceptor()
+    
+    static func fetchProfileImageCustom(image:Data) -> AnyPublisher<NoDataResponse,AFError>{
+        print("프로필 이미지 커스텀 저장 api 호출")
+        let formData = MultipartFormData()
+        formData.append(image, withName: "image", fileName: "\(image).jpg", mimeType: "image/jpg")
+        
+        return ApiClient.shared.session
+            .upload(multipartFormData: formData, with: ProfileImageRouter.profileCustom(image: image),interceptor: intercept)
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: NoDataResponse.self)
+            .value()
+            .map{ receivedValue in
+                print("결과 메세지  : \(receivedValue.message)")
+                return receivedValue.self
+            }
+            .eraseToAnyPublisher()
+    }
+    static func fetchProfileImageDefault(image:String) -> AnyPublisher<NoDataResponse,AFError>{
+        print("프로필 이미지 기본 저장 api 호출")
+        let formData = MultipartFormData()
+        if let textData = image.data(using: .utf8) {
+            formData.append(textData, withName: "image")
+        }
+        return ApiClient.shared.session
+            .upload(multipartFormData: formData, with: ProfileImageRouter.profileDefault(image: image),interceptor: intercept)
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: NoDataResponse.self)
+            .value()
+            .map{ receivedValue in
+                print("결과 메세지  : \(receivedValue.message)")
+                return receivedValue.self
+            }
+            .eraseToAnyPublisher()
+    }
+}
