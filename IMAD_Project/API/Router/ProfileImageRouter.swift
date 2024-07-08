0960e263613b120ae415937fb5f5aaa25103224d
QuaRang <dbduddnd1225@gmail.com>
2024-07-08 13:52:26 +0900

    #114 - 프로필 이미지 저장 요청 관련
    - 프로필 이미지 관련 라우터 생성
    - 앤드포인트 설정
    - 메서드 patch로 고정
    - URLRequest 생성 등

--- IMAD_Project/API/Router/ProfileImageRouter.swift
+++ IMAD_Project/API/Router/ProfileImageRouter.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Alamofire
+
+enum ProfileImageRouter:URLRequestConvertible{
+    
+    case profileCustom(image:Data)
+    case profileDefault(image:String)
+    
+    var baseUrl:URL{
+        return URL(string: ApiClient.baseURL)!
+    }
+    
+    var endPoint:String{
+        switch self{
+        case .profileCustom:
+            return "api/user/profile_image/custom"
+        case .profileDefault:
+            return "api/user/profile_image/default"
+        }
+    }
+    var method:HTTPMethod{
+        return .patch
+    }
+    func asURLRequest() throws -> URLRequest {
+        let url = baseUrl.appendingPathComponent(endPoint)
+        var request = URLRequest(url: url)
+        request.method = method
+        return try URLEncoding(destination: .queryString).encode(request, with: nil)
+    }
+}
