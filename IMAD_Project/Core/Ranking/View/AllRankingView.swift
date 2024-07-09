dc0a0dce7d6594765248aec87a94acdfbfb34347
QuaRang <dbduddnd1225@gmail.com>
2024-07-10 00:44:57 +0900

    #109 - 랭킹/타입 필터 관련
    - 랭킹필터 전체 추가
    - 타입 필터 쿼리 추가

---
+++ IMAD_Project/Core/Ranking/View/AllRankingView.swift
@@ -0,0 +1,18 @@
+//
+//  AllRankingView.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 7/9/24.
+//
+
+import SwiftUI
+
+struct AllRankingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllRankingView()
+}
--- IMAD_Project/Filter/TypeFilter.swift
+++ IMAD_Project/Filter/TypeFilter.swift
@@ -8,15 +8,25 @@
 import Foundation
 
 enum TypeFilter:String,CaseIterable{
-    case animation = "ANIMATION"
-    case tv = "TV"
-    case movie = "MOVIE"
+    case all
+    case tv
+    case movie
+    case animation
     
     var name:String{
         switch self{
         case .animation: return "애니메이션"
         case .tv: return "시리즈"
         case .movie: return "영화"
+        case .all: return "전체"
+        }
+    }
+    var query:String{
+        switch self{
+        case .animation: "ANIMATION"
+        case .tv: "TV"
+        case .movie:"MOVIE"
+        case .all:""
         }
     }
 }
