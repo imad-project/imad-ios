6e13b661c6cadd0fe58a142294bd0ad81fcbfed3
QuaRang <dbduddnd1225@gmail.com>
2024-07-19 23:55:44 +0900

    #82 - 신고하기 필터
    - 신고하기 필터  구현
    - 잘못된 정보
    - 스팸, 상업적 광고
    - 폭력적이거나 공격적인 내용
    - 부적절한 내용
    - 저작권 침해
    - 기타

--- IMAD_Project/Filter/ReportFilter.swift
+++ IMAD_Project/Filter/ReportFilter.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+enum ReportFilter:String,CaseIterable{
+    case wrongInfo = "WRONG_INFO"
+    case spam = "SPAM"
+    case abusive = "ABUSIVE"
+    case inappropriate = "INAPPROPRIATE"
+    case coprightViolation = "COPYRIGHT_VIOLATION"
+    case other = "OTHER"
+    
+    var name:String{
+        switch self{
+        case .wrongInfo:
+            "잘못된 정보"
+        case .spam:
+            "스팸, 상업적 광고"
+        case .abusive:
+            "폭력적이거나 공격적인 내용"
+        case .inappropriate:
+            "부적절한 내용(성적인 컨텐츠, 혐오발언 등)"
+        case .coprightViolation:
+            "저작권 침해"
+        case .other:
+            "기타"
+        }
+    }
+}
