1bb0e43161a2a4aedfe6ee3c4f6ca67c31829683
QuaRang <dbduddnd1225@gmail.com>
2024-07-19 23:53:41 +0900

    #82 - 신고하기 기능 관련
    - 신고하기 APiService 구현(유저/리뷰/게시물/댓글)

--- IMAD_Project/API/Service/ReportApiService.swift
+++ IMAD_Project/API/Service/ReportApiService.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import Alamofire
+import Combine
+
+class ReportApiService{
+    
+    static let interceptor = BaseIntercept()
+    
+    static func user(id:Int,type:String,description:String) -> AnyPublisher<NoDataResponse,AFError>{
+        print("유저 신고 api 호출")
+        return ApiClient.shared.session
+            .request(ReportRouter.user(id: id, type: type, description: description),interceptor: interceptor)
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: NoDataResponse.self)
+            .value()
+            .map{ receivedValue in
+                print("결과 메세지  : \(receivedValue.message)")
+                return receivedValue.self
+            }
+            .eraseToAnyPublisher()
+    }
+    static func review(id:Int,type:String,description:String) -> AnyPublisher<NoDataResponse,AFError>{
+        print("리뷰 신고 api 호출")
+        return ApiClient.shared.session
+            .request(ReportRouter.review(id: id, type: type, description: description),interceptor: interceptor)
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: NoDataResponse.self)
+            .value()
+            .map{ receivedValue in
+                print("결과 메세지  : \(receivedValue.message)")
+                return receivedValue.self
+            }
+            .eraseToAnyPublisher()
+    }
+    static func posting(id:Int,type:String,description:String) -> AnyPublisher<NoDataResponse,AFError>{
+        print("게시글 신고 api 호출")
+        return ApiClient.shared.session
+            .request(ReportRouter.posting(id: id, type: type, description: description),interceptor: interceptor)
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: NoDataResponse.self)
+            .value()
+            .map{ receivedValue in
+                print("결과 메세지  : \(receivedValue.message)")
+                return receivedValue.self
+            }
+            .eraseToAnyPublisher()
+    }
+    static func comment(id:Int,type:String,description:String) -> AnyPublisher<NoDataResponse,AFError>{
+        print("댓글 신고 api 호출")
+        return ApiClient.shared.session
+            .request(ReportRouter.comment(id: id, type: type, description: description),interceptor: interceptor)
+            .validate(statusCode: 200..<300)
+            .publishDecodable(type: NoDataResponse.self)
+            .value()
+            .map{ receivedValue in
+                print("결과 메세지  : \(receivedValue.message)")
+                return receivedValue.self
+            }
+            .eraseToAnyPublisher()
+    }
+}
