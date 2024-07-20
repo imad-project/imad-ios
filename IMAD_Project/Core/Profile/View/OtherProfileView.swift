bd77f53006eb96dc64480064cf8d2339dd5df27f
QuaRang <dbduddnd1225@gmail.com>
2024-07-20 20:42:55 +0900

    #82 - 유저 신고 관련
    - 유저 프로필 화면 구현
    - 유저 프로필 요청 뷰 모델 생성

---
+++ IMAD_Project/Core/Profile/View/OtherProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherProfileView.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 7/20/24.
+//
+
+import SwiftUI
+
+struct OtherProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OtherProfileView()
+}
---
+++ IMAD_Project/Core/Profile/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 7/20/24.
+//
+
+import Foundation
