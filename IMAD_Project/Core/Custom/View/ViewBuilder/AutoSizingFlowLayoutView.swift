161233f1efe8387bad5847fd273123e5d09c31d4
QuaRang <dbduddnd1225@gmail.com>
2024-08-01 20:44:15 +0900

    #127 - 오토 사이징 플로우 레이아웃
    - 파일 생성

---
+++ IMAD_Project/Core/Custom/View/ViewBuilder/AutoSizingFlowLayoutView.swift
@@ -0,0 +1,18 @@
+//
+//  AutoSizingFlowLayoutView.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 8/1/24.
+//
+
+import SwiftUI
+
+struct AutoSizingFlowLayoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AutoSizingFlowLayoutView()
+}
---
+++ IMAD_Project/PreferenceKey/HeightPreferenceKey.swift
@@ -0,0 +1,8 @@
+//
+//  HeightPreferenceKey.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 8/1/24.
+//
+
+import Foundation
