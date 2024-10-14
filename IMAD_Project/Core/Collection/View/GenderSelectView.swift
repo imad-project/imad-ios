93522f2dd0a5c6b615ea83196406019be7153b12
QuaRang <dbduddnd1225@gmail.com>
2024-10-14 10:26:58 +0900

    #135 - 성별 선택 화면 관련
    - 유저정보 공유

--- IMAD_Project/Core/Collection/View/GenderSelectView.swift
+++ IMAD_Project/Core/Collection/View/GenderSelectView.swift
@@ -9,38 +9,32 @@ import SwiftUI
 
 struct GenderSelectView: View {
     
-
+    
     @EnvironmentObject var vm:AuthViewModel
     
     var body: some View {
-        ZStack{
-            Color.white.ignoresSafeArea()
-            VStack(alignment: .leading,spacing: 5){
-                guideView
-                VStack{
-                    genderSelectView(gender: "MALE")
-                    genderSelectView(gender: "FEMALE")
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical)
-                CustomConfirmButton(text: "다음", color: .customIndigo.opacity(0.5),textColor:.white) {
-                    withAnimation(.linear){
-                        vm.selection = .age
-                    }
-                }
-                .padding(.bottom,50)
-                
-                
-            }.foregroundColor(.customIndigo).padding()
-            
+        VStack(alignment: .leading,spacing: 5){
+            guideView
+            VStack{
+                genderSelectView(gender: "MALE")
+                genderSelectView(gender: "FEMALE")
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical)
+            CustomConfirmButton(text: "다음", color: .customIndigo.opacity(0.5),textColor:.white) {
+                withAnimation(.linear){ vm.selection = .age }
+            }
+            .padding(.bottom,50)
         }
+        .foregroundColor(.customIndigo).padding()
+        .background(.white)
     }
 }
 
 struct GenderSelectView_Previews: PreviewProvider {
     static var previews: some View {
         GenderSelectView()
-           
+            .environmentObject(AuthViewModel(user: CustomData.user))
     }
 }
 extension GenderSelectView{
@@ -54,28 +48,26 @@ extension GenderSelectView{
         }
     }
     func genderSelectView(gender:String) -> some View{
-       
-            Button {
-                vm.patchUser.gender = gender
-            } label: {
-                HStack{
-                    Image(gender)
-                        .resizable()
-                        .frame(width: 20,height: 25)
-                    Text(gender == "MALE" ? "남성" : "여성")
-                        .fontWeight(vm.patchUser.gender  == gender ? .bold:.none)
-                    Spacer()
-                }
-                .padding()
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
+        Button {
+            vm.patchUser.gender = gender
+        } label: {
+            HStack{
+                Image(gender)
+                    .resizable()
+                    .frame(width: 20,height: 25)
+                Text(gender == "MALE" ? "남성" : "여성")
+                    .fontWeight(vm.patchUser.gender  == gender ? .bold:.none)
+                Spacer()
             }
-            .overlay {
-                if vm.patchUser.gender == gender{
-                    Color.black.opacity(0.5).cornerRadius(10)
-                }
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(10)
+        }
+        .overlay {
+            if vm.patchUser.gender == gender{
+                Color.black.opacity(0.5).cornerRadius(10)
             }
-            
+        }
     }
 }
 
