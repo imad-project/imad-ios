cba2158e8349dd71d8022e5379bc7b9be4b46659
QuaRang <dbduddnd1225@gmail.com>
2024-10-16 22:03:04 +0900

    #135 - 리니어 뷰
    - 파일 따로 관리

--- IMAD_Project/Extension/LinearGradient.swift
+++ IMAD_Project/Extension/LinearGradient.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension LinearGradient{
+    static var theme:[LinearGradient]{
+        return [
+            LinearGradient(colors: [.green.opacity(0.6),.cyan.opacity(0.6)], startPoint: .topLeading, endPoint: .bottomTrailing),
+            LinearGradient(colors: [.pink.opacity(0.6),.yellow.opacity(0.8)], startPoint: .topLeading, endPoint: .bottomTrailing),
+            LinearGradient(colors: [.gray.opacity(0.4),.gray.opacity(0.9)], startPoint: .topLeading, endPoint: .bottomTrailing),
+            LinearGradient(colors: [.purple.opacity(0.7),.red.opacity(0.6)], startPoint: .topLeading, endPoint: .bottomTrailing),
+            LinearGradient(colors: [.brown.opacity(0.5),.orange.opacity(0.6)], startPoint: .topLeading, endPoint: .bottomTrailing)
+        ].shuffled()
+    }
+}
