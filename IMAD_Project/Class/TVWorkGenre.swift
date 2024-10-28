29088c10bc3ab9bb1339d161c8c5d29e985790b5
QuaRang <dbduddnd1225@gmail.com>
2024-10-28 15:19:43 +0900

    #135 - TV 및 Movie 장르 관련
    - 같은 파일로 관리 되어있던 코드 다른 파일로 분리

---
+++ IMAD_Project/Class/TVWorkGenre.swift
@@ -0,0 +1,22 @@
+//
+//  TVWorkGenre.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 10/28/24.
+//
+
+import Foundation
+
+class TVWorkGenre:WorkGenre{
+    var genreType: WorkGenreType = .tv
+    var tvGenre:RecommendTVResponse
+    required init(tvGenre:RecommendTVResponse) {
+        self.tvGenre = tvGenre
+    }
+    var id:Int{ tvGenre.id }
+    var name:String?{ tvGenre.name }
+    var title:String?
+    var genreId:[Int]?{ tvGenre.genreIds }
+    var posterPath:String?{ tvGenre.posterPath }
+    var backdropPath:String?{ tvGenre.backdropPath }
+}
