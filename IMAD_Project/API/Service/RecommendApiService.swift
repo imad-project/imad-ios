6372e46e40ef0beaa4fdf1e63020049d9f2285bf
QuaRang <dbduddnd1225@gmail.com>
2024-11-04 20:28:38 +0900

    #136 - 작품 추천 네트워크 요청 관련
    - RecommendApiService : 기존에 추천별로 다른 메서드 존재하던 것 제네릭 시용으로 한 메서드로 변경

--- IMAD_Project/API/Service/RecommendApiService.swift
+++ IMAD_Project/API/Service/RecommendApiService.swift
@@ -21,39 +21,12 @@ class RecommendApiService{
             .value()
             .eraseToAnyPublisher()
     }
-    static func genre(page:Int,type:String) -> AnyPublisher<GenreRecommend,AFError>{
-        print("장르 기반 작품 추천 api 호출")
+    static func list<T:Decodable>(page:Int,type:String,contentsId:Int? = nil,category:String? = nil,recommendListType:RecommendListType) -> AnyPublisher<T,AFError>{
+        print("\(recommendListType.endPoint) 작품 추천 api 호출")
         return ApiClient.shared.session
-            .request(RecommendRouter.genre(page: page,type: type),interceptor: interseptor)
+            .request(RecommendRouter.list(page: page, type: type, contentsId: contentsId, category: category, recommendListType: recommendListType),interceptor: interseptor)
             .validate(statusCode: 200..<300)
-            .publishDecodable(type:GenreRecommend.self)
-            .value()
-            .eraseToAnyPublisher()
-    }
-    static func activity(page:Int,contentsId:Int) -> AnyPublisher<ActivityRecommend,AFError>{
-        print("활동 기반 작품 추천 api 호출")
-        return ApiClient.shared.session
-            .request(RecommendRouter.activity(page: page,contentsId:contentsId),interceptor: interseptor)
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type:ActivityRecommend.self)
-            .value()
-            .eraseToAnyPublisher()
-    }
-    static func imad(page:Int,type:String,category:String) -> AnyPublisher<ImadRecommend,AFError>{
-        print("Imad 작품 추천 api 호출")
-        return ApiClient.shared.session
-            .request(RecommendRouter.imad(page: page,type: type,category: category),interceptor: interseptor)
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type:ImadRecommend.self)
-            .value()
-            .eraseToAnyPublisher()
-    }
-    static func trend(page:Int,type:String) -> AnyPublisher<TrendRecommend,AFError>{
-        print("대세 작품 추천 api 호출")
-        return ApiClient.shared.session
-            .request(RecommendRouter.trend(page: page,type: type),interceptor: interseptor)
-            .validate(statusCode: 200..<300)
-            .publishDecodable(type:TrendRecommend.self)
+            .publishDecodable(type:T.self)
             .value()
             .eraseToAnyPublisher()
     }
