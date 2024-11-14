7da820d902615415f360f518a586123fc449e9e6
QuaRang <dbduddnd1225@gmail.com>
2024-11-14 16:34:13 +0900

    - NavigationViewModel 생성
    - View의 이동을 자유자제로 컨트롤하기 위해 만든 ViewModel
    - 이런저런 테스트 진행 후 적용 예정

---
+++ IMAD_Project/Core/Menu/ViewModel/NavigationViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationViewModel.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 11/14/24.
+//
+
+import Foundation
+import SwiftUI
+
+class NavigationViewModel:ObservableObject{
+    @Published var view:AnyView? = nil
+}
