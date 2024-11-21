464906a124016c85e597a6c3b4823829bf8dbfe8
QuaRang <dbduddnd1225@gmail.com>
2024-11-21 22:41:33 +0900

    #144 - 리뷰 관련 캐시 및 매니저 구현 - 리뷰 리스트 정보를 담을 캐시 DTO 구현 - 캐시 저장 및 만료시간 캐싱 생성 매니져 구현

---
+++ IMAD_Project/Cache/Cache/ReviewCache.swift
@@ -0,0 +1,15 @@
+//
+//  ReviewCache.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 11/21/24.
+//
+
+import Foundation
+
+struct ReviewCache{
+    var id:Int
+    var maxPage:Int
+    var currentPage:Int
+    var list:[ReviewResponse]
+}
---
+++ IMAD_Project/Cache/CacheManager/ReviewCacheManager.swift
@@ -0,0 +1,46 @@
+//
+//  ReviewCacheManager.swift
+//  IMAD_Project
+//
+//  Created by 유영웅 on 11/21/24.
+//
+
+import Foundation
+
+class ReviewCacheManager{
+    static let instance = ReviewCacheManager()
+    private let lock = NSLock()
+    private init(){}
+    
+    var reviewStorage = [String:ReviewResponse]()
+    var reviewListStorage = [String:ReviewCache]()
+    var reviewTimeStamp = [String:Date]()
+    var reviewListTimeStamp = [String:Date]()
+    
+    func reviewCachedData(key:String)->ReviewResponse?{
+        guard let data = reviewStorage[key] else { return nil }
+        return data
+    }
+    func reviewListCachedData(key:String)->ReviewCache?{
+        guard let data = reviewListStorage[key] else { return nil }
+        return data
+    }
+    func reviewOfUpdateData(data:ReviewResponse?){
+        DispatchQueue.global(qos:.background).async {
+            self.lock.lock()
+            guard let data else {return}
+            self.reviewTimeStamp["\(data.reviewID)"] = Date()
+            self.reviewStorage.updateValue(data,forKey:"\(data.reviewID)")
+            self.lock.unlock()
+        }
+    }
+    func reviewListOfUpdateData(data:ReviewCache?){
+        DispatchQueue.global(qos:.background).async {
+            self.lock.lock()
+            guard let data else {return}
+            self.reviewListTimeStamp["\(data.id)"] = Date()
+            self.reviewListStorage.updateValue(data,forKey:"\(data.id)")
+            self.lock.unlock()
+        }
+    }
+}
